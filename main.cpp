24949a89e8633c8edf6250529a30ea5255522552
minisephirot <minisephirot@hotmail.fr>
2016-09-26 11:48:58 +0200

    Ajout de la touche echap + commentaire

--- main.cpp
+++ main.cpp
@@ -3,39 +3,42 @@
 #include <GL/glu.h>
 #include <cstdlib>
 #include "lib_sdl_fst.h"
-
-//blablabla
-
-void sdl3d(int x, int y, int c);
+// Récupère les libraires OpenGL/SDL
 
 int main(int argc, char *argv[])
 {
     SDL_Event event;
     sdl3d(600,600, 100);
     SDL_WM_SetCaption("Base 3D", NULL);
+    int exit=1;
 
-    for(;;)
+    while(exit) //Permet de gérer les inputs de l'utilisateurs
     {
         SDL_WaitEvent(&event);
 
-        switch(event.type)
+        switch(event.key.keysym.sym)
         {
-            case SDL_QUIT:
-              exit(0);
-            break;
+            case SDLK_ESCAPE: //On appuie sur echap, on sort de la boucle et quitte le jeu
+                exit = 0;
+                break;
+            case SDLK_z:
+                camera(3,4,3,0,0,0);
+                break;
+            default:
+                break;
         }
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
         camera(3,4,3,0,0,0);
 
         glPushMatrix();
-        cube(1,0,0,0,50,0,0);    
+        cube(1,0,0,0,50,0,0);
         cube(1,-3,-3,0,0,0,0);
         glPopMatrix();
 
         glPushMatrix();
         cube(1,-3,-3,0,0,0,0);
         glPopMatrix();
-        
+
 
         glFlush();
         SDL_GL_SwapBuffers();
@@ -44,8 +47,6 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
-
 
 
 
