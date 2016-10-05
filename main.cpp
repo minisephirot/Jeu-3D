735bedf3a9b18504ba38f0e73023f3adb2dfd45f
TamStam <tamashipoub@gmail.com>
2016-10-05 17:38:34 +0200

    orientation de la camera

--- main.cpp
+++ main.cpp
@@ -2,67 +2,78 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <cstdlib>
+#include <stdio.h>
+#include <math.h>
 #include "lib_sdl_fst.h"
-// Récupère les libraires OpenGL/SDL
+
 
 int main(int argc, char *argv[])
 {
-    SDL_Event event;
-    sdl3d(800,600, 100);
-    SDL_WM_SetCaption("Base 3D", NULL);
-    int exit=1;
+/*  *****************
+    * Les Variables *
+    *****************  */
 
     int x = 19;
     int y = 19;
     double a = 0;
     double b = 0;
 
-    SDL_EnableKeyRepeat(10, 10); //Permet la répétition des entrées de clavier en régénérant l'evenement de touche préssée toute les 10ms
-    while(exit) //Permet de gérer les inputs de l'utilisateurs
+// initialisation
+    SDL_Event event;
+    sdl3d(800,600, 100);
+    SDL_WM_SetCaption("Base 3D", NULL);
+    int exit=1;
+
+
+
+    SDL_EnableKeyRepeat(10, 10); //Permet la répétition des entrées de clavier toute les 10ms
+    while(exit) 
     {
         if(SDL_PollEvent(&event)){
          switch(event.key.keysym.sym){
-            case SDLK_ESCAPE: //On appuie sur echap, on sort de la boucle et quitte le jeu
+            // Fin du jeu : echap ou fermé
+            case SDL_QUIT:
+                exit = 0;
+            break;
+            case SDLK_ESCAPE: 
                 exit = 0;
                 break;
-            case SDLK_w: // Si l'utilisateur appuie sur Z, faire avancer la caméra (axe x)
+            //Déplcement de la caméra sur l'axe x et y
+            case SDLK_z: 
                 x=x+2;
                 break;
-            case SDLK_s: // Si l'utilisateur appuie sur S, faire reculer la caméra (axe x)
+            case SDLK_s: 
                 x=x-2;
                 break;
-            case SDLK_a: // Si l'utilisateur appuie sur Q, faire aller la caméra a gauche (axe y)
+            case SDLK_q: 
                 y=y+2;
                 break;
-            case SDLK_d: // Si l'utilisateur appuie sur Z, faire aller la caméra a droite (axe y)
+            case SDLK_d: 
                 y=y-2;
                 break;
-            case SDLK_UP: // Si l'utilisateur appuie sur Fleche haut, faire pivoter la caméra vers le haut (axe z)
-                if (a < 10){
-                    a=a+1;
-                }
+            //orientation de la caméra sur l'axe x et y
+            case SDLK_LEFT:
+                b=b-0.1;
+                printf ("%f   ", b);
                 break;
-            case SDLK_DOWN: // Si l'utilisateur appuie sur fleche bas , faire pivoter la caméra vers le bas (axe z)
-                if (a > -10){
-                    a=a-1;
-                }
-                break;
-            case SDLK_LEFT: // Si l'utilisateur appuie sur fleche gauche, faire pivoter la caméra vers la gauche (axe y)
-                b=b+1;
-                break;
-            case SDLK_RIGHT: // Si l'utilisateur appuie sur fleche droite, faire pivoter la caméra vers la droite (axe y)
-                b=b-1;
+            case SDLK_RIGHT: 
+                b=b+0.1;
                 break;
             default:
                 break;
          }
         }
+    // La variable b n'a pas besoin d'etre plus grand que 2PI
+    if (b>2*3.14)
+      b = 0;
+    if (b<-2*3.14)
+      b=0;
+
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+        
+        camera(x,y,20,x-cos(b)*20,y+sin(b)*20,20);
 
-        camera(x,y,20,x+3,y+b,20+a);
-        glPopMatrix();
-
-        // Boucle qui forme les cubes utilisés pour "parquer" le joueur (fais à deux sur mon PC)
+        // creation du plateau
         int i=0;
         while(i<=15){
             cube_position(18,15*38,i*38,0,0,0,0);
