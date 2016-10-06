e52f1fd9f21d54054da55e1c33c78ac17eb7e6d0
minisephirot <minisephirot@hotmail.fr>
2016-10-06 15:12:17 +0200

    Plusieurs inputs MAIS retour soucis trigo

--- main.cpp
+++ main.cpp
@@ -17,10 +17,10 @@
 #include "lib_sdl_fst.h"
 
 // Les define :
-#define TAILLE_PLATEAU 15 
+#define TAILLE_PLATEAU 15
 #define TAILLE_CUBES 38
-#define VITESSE_DEPLACEMENT 5
-#define VITESSE_ORIENTATION 5
+#define VITESSE_DEPLACEMENT 2
+#define VITESSE_ORIENTATION 2
 
 
 int main(int argc, char *argv[])
@@ -28,16 +28,23 @@ int main(int argc, char *argv[])
 /*  *****************
     * Les Variables *
     *****************  */
-    
+
     // variable annoncant la fin de jeu
-    int exit; 
-    
+    int exit;
+
     //Position de la camera
     int x, y;
- 
-    //direction
-    double b,a;
 
+    //direction
+    double b;
+
+    bool w=false;
+    bool s=false;
+    bool a=false;
+    bool d=false;
+    bool esc=false;
+    bool fgauche=false;
+    bool fdroite=false;
 
 /*  *****************
     *     Debut     *
@@ -54,54 +61,54 @@ int main(int argc, char *argv[])
     SDL_Event event;
     sdl3d(800,600, 100);
     SDL_WM_SetCaption("Base 3D", NULL);
-    
-
-
 
     SDL_EnableKeyRepeat(10, 10); //Permet la répétition des entrées de clavier toute les 10ms
 
     /************************
      *     Debut du jeu     *
      ************************/
-    while(exit) 
+    while(exit)
     {
-
         //---------Début Des Events---------
-        if(SDL_PollEvent(&event)){
-         switch(event.key.keysym.sym){
+        while (SDL_PollEvent(&event)){
+            Uint8 *keystates = SDL_GetKeyState(NULL);
+
+
+            w = keystates[SDLK_w];
+            s = keystates[SDLK_s];
+            a = keystates[SDLK_a];
+            d = keystates[SDLK_d];
+            esc = keystates[SDLK_ESCAPE];
+            fgauche = keystates[SDLK_LEFT];
+            fdroite = keystates[SDLK_RIGHT];
+
             // Fin du jeu : echap ou fermé
-            case SDLK_ESCAPE: 
-                exit = 0;
-                break;
-
+            if (esc){
+            exit=0;
+            }
             //Déplcement de la caméra sur l'axe x et y
-            case SDLK_z: 
-                x=x+cos(b*M_PI/180)*VITESSE_DEPLACEMENT;
-                y=y-sin(b*M_PI/180)*VITESSE_DEPLACEMENT;
-                break;
-            case SDLK_s: 
-                x=x-cos(b*M_PI/180)*VITESSE_DEPLACEMENT;
-                y=y+sin(b*M_PI/180)*VITESSE_DEPLACEMENT;
-                break;
-            case SDLK_q: 
-                x=x+cos((b-90)*M_PI/180)*VITESSE_DEPLACEMENT;
-                y=y-sin((b-90)*M_PI/180)*VITESSE_DEPLACEMENT;
-                break;
-            case SDLK_d: 
-                x=x+cos((b+90)*M_PI/180)*VITESSE_DEPLACEMENT;
-                y=y-sin((b+90)*M_PI/180)*VITESSE_DEPLACEMENT;
-                break;
-
-            //orientation de la caméra sur l'axe x et y
-            case SDLK_LEFT:
-                b=b-VITESSE_ORIENTATION;          
-                break;
-            case SDLK_RIGHT: 
-                b=b+VITESSE_ORIENTATION;
-                break;
-            default:
-                break;
-        }
+            if (w){
+            x=x+cos(b*M_PI/180)*VITESSE_DEPLACEMENT;
+            y=y-sin(b*M_PI/180)*VITESSE_DEPLACEMENT;
+            }
+            if (s){
+            x=x-cos(b*M_PI/180)*VITESSE_DEPLACEMENT;
+            y=y+sin(b*M_PI/180)*VITESSE_DEPLACEMENT;
+            }
+            if (a){
+            x=x+cos((b-90)*M_PI/180)*VITESSE_DEPLACEMENT;
+            y=y-sin((b-90)*M_PI/180)*VITESSE_DEPLACEMENT;
+            }
+            if (d){
+            x=x+cos((b+90)*M_PI/180)*VITESSE_DEPLACEMENT;
+            y=y-sin((b+90)*M_PI/180)*VITESSE_DEPLACEMENT;
+            }
+            if (fgauche){
+            b=b-VITESSE_ORIENTATION;
+            }
+            if (fdroite){
+            b=b+VITESSE_ORIENTATION;
+            }
       }
       //---------Fin des Events---------
 
@@ -111,10 +118,10 @@ int main(int argc, char *argv[])
         b = 0;
       if (b<-1)
         b=359;
-  
-      glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );    
-      camera(x,y,20,x+cos(b*M_PI/180)*20,y-sin(b*M_PI/180)*20,20+a); //explication en annexe 
-  
+
+      glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+      camera(x,y,20,x+cos(b*M_PI/180)*20,y-sin(b*M_PI/180)*20,20); //explication en annexe
+
 
       //---------Debut Du Plateau---------
       int i=0;
