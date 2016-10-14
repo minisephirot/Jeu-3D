78f4238b222b796f98e8a42ada154addd4e2971c
Remi Le Brech <lebrech2u@fst-ats-311-l14.fst.site.univ-lorraine.fr>
2016-10-14 15:40:59 +0200

    Ajout ennemis.h

---
+++ ennemis.h
@@ -0,0 +1,38 @@
+#include "constante.h"
+
+void deplcementEnnemis(Ennemis *cube, int ncubes)
+{
+	for (int i=0; i<ncubes; i++) {
+      //---------Géstion des Ennemis---------
+      //****************************************
+
+          cube[i].x+=cube[i].dx;
+          cube[i].y+=cube[i].dy;
+          //---gestion mouvement---
+          if (cube[i].x >= ((TAILLE_CUBES*TAILLE_PLATEAU)-TAILLE_CUBES))
+          {
+		cube[i].dx *= -1;
+          }
+          if (cube[i].x <= TAILLE_CUBES)
+          {
+		cube[i].dx *= -1;
+          }
+
+          if (cube[i].y <= TAILLE_CUBES)
+          {
+		cube[i].dy *= -1;
+          }
+          if(cube[i].y >= ((TAILLE_CUBES*TAILLE_PLATEAU)-TAILLE_CUBES))
+          {
+		cube[i].dy *= -1;
+          }
+          //---fin gestion---
+
+
+
+          cube_position(TAILLE_CUBES/2,cube[i].x,cube[i].y,0,90,0,0);
+
+	}
+      //---------Fin de géstion---------
+      //**************************************
+}
