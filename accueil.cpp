c490dc2db7a3bf8f6981b10df8d38166a373cc6e
TamStam <tamashipoub@gmail.com>
2016-11-14 16:08:51 +0100

    Création d'en-tete

---
+++ accueil.cpp
@@ -0,0 +1,118 @@
+#include "accueil.h"
+int menu(){
+
+  bool continuer;
+  int choix;
+  SDL_Surface *ecran = NULL,
+  *fond = NULL,
+  *fleche = NULL;
+  SDL_Rect position, posFleche;
+
+
+  SDL_Init(SDL_INIT_VIDEO);
+  ecran = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE);
+  SDL_WM_SetCaption("NOM DU JEU", NULL);
+  SDL_EnableKeyRepeat(0,0);
+  position.x = 0;
+  position.y = 0;
+  posFleche.x= 330;
+  posFleche.y= 150;
+
+  choix = 1;
+
+  fond = SDL_LoadBMP("texture/cube.bmp");
+  fleche = SDL_LoadBMP("texture/fleche.bmp");
+  SDL_SetColorKey(fleche, SDL_SRCCOLORKEY, SDL_MapRGB(fleche->format, 0, 255, 0));
+
+  continuer = true;
+  SDL_Event event;
+  while (continuer){
+    SDL_WaitEvent(&event);
+    switch(event.type)
+    {
+      case SDL_QUIT:
+        continuer = 0;
+        choix = 0;
+      break;
+      case SDL_KEYDOWN:
+        switch (event.key.keysym.sym)
+        {
+          case SDLK_ESCAPE:
+            continuer = 0;
+            choix = 0;
+          break;
+          case SDLK_SPACE:
+            continuer = 0;
+          break;
+          case SDLK_RETURN:
+            continuer = 0;
+          break;
+          case SDLK_DOWN:
+            if (choix == 1)
+            {
+            posFleche.x= 330;
+            posFleche.y= 220;
+            choix = 2;
+            SDL_Delay(100);
+            }
+            else
+              if (choix == 2)
+              {
+                posFleche.x= 330;
+                posFleche.y= 300;
+                choix = 3;
+                SDL_Delay(100);
+              }
+              else
+                if (choix == 3)
+                {
+                  posFleche.x= 330;
+                  posFleche.y= 150;
+                  choix = 1;
+                  SDL_Delay(100);
+                }
+          break;
+          case SDLK_UP:
+            if (choix == 1)
+            {
+            posFleche.x= 330;
+            posFleche.y= 300;
+            choix = 3;
+            SDL_Delay(100);
+            }
+            else
+              if (choix == 2)
+              {
+                posFleche.x= 330;
+                posFleche.y= 150;
+                choix = 1;
+                SDL_Delay(100);
+              }
+              else
+                if (choix == 3)
+                {
+                  posFleche.x= 330;
+                  posFleche.y= 220;
+                  choix = 2;
+                  SDL_Delay(100);
+
+                }
+          default:
+          break;
+        }
+        default:
+        break;
+    }
+
+    SDL_BlitSurface(fond, NULL, ecran, &position);
+    SDL_BlitSurface(fleche, NULL, ecran, &posFleche);
+
+    SDL_Flip(ecran);
+
+  }
+
+  SDL_FreeSurface(fond);
+  SDL_FreeSurface(fleche);
+
+  return choix;
+}
---
+++ ennemis.cpp
@@ -0,0 +1,48 @@
+#include "ennemis.h"
+void deplcementEnnemis(Ennemis *cube, int ncubes, int scorejoueur){
+	for (int i=0; i<ncubes; i++) {
+      //---------Géstion des Ennemis---------
+      //****************************************
+
+          cube[i].x+=cube[i].dx * (scorejoueur/40) ;
+          cube[i].y+=cube[i].dy * (scorejoueur/40) ;
+          //---gestion mouvement---
+          if (cube[i].x >= ((TAILLE_CUBES*TAILLE_PLATEAU)-TAILLE_CUBES))
+          {
+		cube[i].dx *= -1 ;
+          }
+          if (cube[i].x <= TAILLE_CUBES)
+          {
+		cube[i].dx *= -1 ;
+          }
+
+          if (cube[i].y <= TAILLE_CUBES)
+          {
+		cube[i].dy *= -1 ;
+          }
+          if(cube[i].y >= ((TAILLE_CUBES*TAILLE_PLATEAU)-TAILLE_CUBES))
+          {
+		cube[i].dy *= -1 ;
+          }
+          //---fin gestion---
+
+
+
+          cube_position(TAILLE_CUBES/2,cube[i].x,cube[i].y,0,90,0,0);
+
+	}
+      //---------Fin de géstion---------
+      //**************************************
+}
+
+
+
+void generationbonus(Bonus *scoring, int nbonus){
+ for (int i=0; i<nbonus; i++) {
+
+
+
+     sphere_position(scoring[i].x,scoring[i].y,scoring[i].z,1,1,1);
+
+	}
+}
